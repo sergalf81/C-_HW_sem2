55ffa1e7e952c4fd8f077e95f02bfec87b2fc136
Sergey A <sergalf81@mail.ru>
2023-09-25 08:35:25 +0300

    Написана программа для первого ДЗ

---
+++ C#_HW_1/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.Write("Input three-digit nimber: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num >= 100 && num < 1000)
+{
+    System.Console.WriteLine($"Second digit of {num} -> {num%100/10}");
+}
+else
+{
+    System.Console.WriteLine("You input not tree-digit number");
+}
