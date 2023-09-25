c580cc35ffee431db554c6897649e228a86d0208
Sergey A <sergalf81@mail.ru>
2023-09-25 11:12:18 +0300

    Создана программа для третьего ДЗ

---
+++ C#_HW_3/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Need to find out the day off or is it not");
+System.Console.Write("Enter the number of the day of the week from 1 to 7: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num >=6 && num <= 7)
+    {
+        System.Console.WriteLine("Yes, it's a day off");
+    }
+else if (num >= 1 && num <= 5)
+    {
+        System.Console.WriteLine("No, it's not a day off");
+    }
+else 
+{
+    System.Console.WriteLine("Enter a valid day between 1 and 7");
+}
+    
