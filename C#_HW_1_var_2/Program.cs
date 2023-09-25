656bb241540a3fd9b920ace8bf4e178256a9ad85
Sergey A <sergalf81@mail.ru>
2023-09-25 09:24:37 +0300

    Добавлен второй вариант со случайными числами к первому ДЗ

---
+++ C#_HW_1_var_2/Program.cs
@@ -0,0 +1,7 @@
+﻿void SecondDigit(int num)
+{
+    int SecondDigit = num%100/10;
+    System.Console.WriteLine(($"{num} -> {num%100/10}"));
+}
+int num = new Random().Next(100, 1000);
+SecondDigit(num);
