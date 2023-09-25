b95488196fb2040f144e083c8464bd2b617bfcc9
Sergey A <sergalf81@mail.ru>
2023-09-25 10:12:38 +0300

    Создана программа для второго ДЗ

---
+++ C#_HW_2/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.Write("Input your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num > 99)
+{
+    string str = num.ToString();
+    System.Console.Write("The third number: ");
+    System.Console.WriteLine(str[2]);
+}
+else
+{
+    System.Console.WriteLine("The third number is missing");
+}
